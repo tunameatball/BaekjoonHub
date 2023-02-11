2845788e21778e4af456feddc8ec1a56f00c1815
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-02-11 16:19:57 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,9 @@
+try {
+    const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+    const a = parseInt(input[0])
+    const b = parseInt(input[1])
+
+    console.log(a / b);
+} catch (error) {
+    
+}
