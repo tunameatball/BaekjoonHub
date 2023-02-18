98658fd3c6b02adde3ff23ba3fa4e51d2d12d8e9
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-02-18 21:14:45 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,13 +1,17 @@
 function solution(ingredient) {
+    let stk = [];
     let count = 0;
-
     for (let i = 0; i < ingredient.length; i++) {
-        if (ingredient.slice(i, i + 4).join('') === '1231') {
+        stk.push(ingredient[i]);
+        if (
+            stk[stk.length-1] === 1 &&
+            stk[stk.length-2] === 3 &&
+            stk[stk.length-3] === 2 &&
+            stk[stk.length-4] === 1
+        ) {
             count++;
-            ingredient.splice(i, 4);
-            i -= 3;
+            stk.splice(-4);
         }
     }
-
     return count;
 }
