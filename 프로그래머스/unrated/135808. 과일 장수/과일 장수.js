a2e28afc54282f88ada807973b0ecbfcb9e19188
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-02-27 18:34:35 +0900

    [unrated] Title: 과일 장수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -5,11 +5,11 @@ function solution(k, m, score) {
   });
 
   const scores = Object.keys(apples).sort();
-  return scores.reduceRight((arr ,score, index) => {
-    arr += parseInt(apples[score] / m) * score;
+  return scores.reduceRight((acc ,score, index) => {
+    acc += parseInt(apples[score] / m) * score;
     if (index != 0) {
       apples[scores.at(index-1)] += apples[score] % m;
     }
-    return arr;
+    return acc;
   }, 0) * m;
 }
