9cf981bba8d4da6f6bc0380a3149ec67c5e4c20c
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-03-20 11:56:22 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,9 @@
+function solution(numlist, n) {
+    return numlist.sort((a, b) => {
+        if (Math.abs(n - a) == Math.abs(n - b)) {
+            return b - a;
+        } else {
+            return Math.abs(n - a) - Math.abs(n - b);
+        }
+    })
+}
