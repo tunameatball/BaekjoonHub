8cb7eb39bae9e8bc7ae086ed94116a1a0f21c04c
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-03-25 16:43:38 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,11 +1,7 @@
 function solution(array) {
     var answer = 0;
     const counts = array.reduce((acc, value) => {
-        if (acc[value] == undefined) {
-            acc[value] = 1;
-        } else {
-            acc[value] += 1;
-        }
+        acc[value] = (acc[value] || 0) + 1;
         
         return acc;
     }, {});
