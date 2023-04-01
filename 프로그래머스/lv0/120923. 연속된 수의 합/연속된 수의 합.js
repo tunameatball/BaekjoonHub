e2c7c01d9b35c882aa99c8a6847b98ec122a3dcf
KwangHee <79885664+tunameatball@users.noreply.github.com>
2023-04-01 15:07:29 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -2,19 +2,18 @@ function solution(num, total) {
     var answer = [];
     const center = Math.floor(total / num);
     answer.push(center);
+    let count;
     if (num % 2 === 0) {
-        const count = Math.floor(num / 2) - 1;
-        for (let i = 1; i <= count; i++) {
-            answer.push(center - i);
-            answer.push(center + i);
-        }
+        count = Math.floor(num / 2) - 1;
         answer.push(center + count + 1);
     } else {
-        const count = Math.floor(num / 2);
-        for (let i = 1; i <= count; i++) {
-            answer.push(center - i);
-            answer.push(center + i);
-        }
+        count = Math.floor(num / 2);
     }
+    
+    for (let i = 1; i <= count; i++) {
+        answer.push(center - i);
+        answer.push(center + i);
+    }
+    
     return answer.sort((a, b) => a - b);
 }
